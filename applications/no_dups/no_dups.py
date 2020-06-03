7ebbb7d61382f8aa047091f66ffa07829b84dc7c
pgambino-espear <pgambino@espear.com>
2020-06-03 19:32:28 -0400

    Did first 4 problems in applications.

--- applications/no_dups/no_dups.py
+++ applications/no_dups/no_dups.py
@@ -1,6 +1,19 @@
 def no_dups(s):
-    # Your code here
-
+    cache = {}
+    arr = s.split()
+    resArr = []
+    for i in range(0, len(arr)):
+        key = arr[i]
+        # print(f"arr[i] is {key}")
+        if arr[i] not in cache:
+            # print(f"The cache is {cache} and arr[i] {arr[i]} is not in there")
+            cache[key] = 1
+            resArr.append(arr[i])
+        else:
+            continue
+    # print(resArr)
+    resStr = " "
+    return resStr.join(resArr)
 
 
 if __name__ == "__main__":
